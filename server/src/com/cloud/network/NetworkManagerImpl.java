479129bd54c8d5403b2290cd868020210b6bab68
Saksham Srivastava <saksham.srivastava@citrix.com>
2013-01-26 19:48:48 +0530

    All working, implement re-dhcp

--- server/src/com/cloud/network/NetworkManagerImpl.java
+++ server/src/com/cloud/network/NetworkManagerImpl.java
@@ -1430,6 +1430,7 @@ public class NetworkManagerImpl implements NetworkManager, Manager, Listener {
             _networksDao.update(networkId, network);
 
             Network result = guru.implement(network, offering, dest, context);
+            network.setGuestCidr(result.getGuestCidr());
             network.setCidr(result.getCidr());
             network.setBroadcastUri(result.getBroadcastUri());
             network.setGateway(result.getGateway());
@@ -1986,6 +1987,7 @@ public class NetworkManagerImpl implements NetworkManager, Manager, Listener {
 
         if (cidr != null && gateway != null) {
             userNetwork.setCidr(cidr);
+            userNetwork.setGuestCidr(cidr);
             userNetwork.setGateway(gateway);
             if (vlanId != null) {
                 userNetwork.setBroadcastUri(URI.create("vlan://" + vlanId));
